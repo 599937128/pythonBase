944aa60afa8e1d9e8b0951a9722bc605e3c64bd0
lvshao <599937128@163.com>
2020-05-28 22:06:43 +0800

    类似java 中的toString 方法

---
+++ src/base6/day04.py
@@ -0,0 +1,13 @@
+class Person():
+    def __init__(self, name, age, height):
+        self.name = name
+        self.age = age
+        self.height = height
+
+    # 相当于  java中的 toString
+    def __str__(self):
+        return "object"
+
+
+p1 = Person("lvliao", 21, 175)
+print(p1)
