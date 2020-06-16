ffacfec356914cb7329de9e12c094616182a1672
lvshao <599937128@163.com>
2020-06-16 22:03:20 +0800

    删除对象的引用解释说明

--- src/base7/day03.py
+++ src/base7/day03.py
@@ -5,9 +5,12 @@ class User:
     def __del__(self):
         print("======对象即将被回收,释放内存空间(内存回收)")
 
+
 u1 = User()
 u2 = u1
+# 删除u1的引用
 del u1
-print("===="*30)
+print("====" * 30)
+# 删除u2的引用 对象的引用为空
 del u2
-print("*"*60)
+print("*" * 60)
