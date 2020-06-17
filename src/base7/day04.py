0ed85b69cbd5655052d86538741b82e027b54987
lvshao <599937128@163.com>
2020-06-17 22:11:58 +0800

    面向对象继承

---
+++ src/base7/day04.py
@@ -0,0 +1,36 @@
+class Animal:
+    def __init__(self):
+        self.name = '动物'
+
+    def eat(self):
+        print("=======吃饭=======")
+
+    def sleep(self):
+        print("=======睡觉========")
+
+
+# 继承 继承父类的构造方法
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def shout(self):
+        print("=======大叫========")
+
+
+class Cat(Animal):
+    # 调用自己的init方法 会覆盖父类的init的方法
+    # def __init__(self):
+    #     print('猫妖厨师花了')
+    def catch(self):
+        print("=======抓老鼠=======")
+
+
+dog = Dog('小白')
+dog.eat()
+dog.shout()
+
+cat = Cat()
+print(cat.name)
+cat.eat()
+cat.catch()
