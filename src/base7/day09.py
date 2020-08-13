1749dec85253091a5e79513245ccb9892046f394
lvshao <599937128@163.com>
2020-08-13 21:38:54 +0800

    静态方法

--- src/base7/day09.py
+++ src/base7/day09.py
@@ -10,7 +10,14 @@ class A(object):
         cls.name = '李四'
         print("---------A的test2方法--类方法")
 
+    @staticmethod    # 静态方法 属于类 没有默认传递的参数  可以通过类对象和类名调用
+    def test3():
+        # 静态方法修改类属性
+        A.name = '王五'
+        print("--------A的test3方法----静态方法")
+
 a = A()
 a.test2()
 A.test2()
+A.test3()
 print(A.name)
