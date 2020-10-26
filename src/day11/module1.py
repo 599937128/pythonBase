fa833349405c87e8a1cec56d1886e30959c08ac3
lvshao <599937128@163.com>
2020-10-26 20:51:46 +0800

    坦克显示

--- src/day11/module1.py
+++ src/day11/module1.py
@@ -12,6 +12,7 @@ class TankeMain(object):
         screen = pygame.display.set_mode((600, 400), 0, 32)
         # 给窗口设置一个标题
         pygame.display.set_caption('坦克大战')
+        my_tank = Tank(screen, 250, 290)
         while True:
             # EGB color (0,100,200) 设置屏幕的背景色
             screen.fill((0, 0, 0))
@@ -19,6 +20,7 @@ class TankeMain(object):
             screen.blit(self.write_text(), (0, 10))
             # 获取事件
             self.get_event()
+            my_tank.display()
             # 显示重置
             pygame.display.update()
 
@@ -53,6 +55,39 @@ class TankeMain(object):
         font = pygame.font.SysFont("simsunexttb", 20)  # 定义一个字体
         text_sf = font.render("敌方坦克数量:10", True, (255, 0, 0))  # 显示文字
         return text_sf
+
+
+# 坦克大战游戏所有对象的父类
+class BaseItem(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        # 所有对象的公共属性
+
+
+class Tank(BaseItem):
+    # 定义类属性,所有的坦克高和宽度都是一致
+    width = 60
+    height = 60
+
+    def __init__(self, screen, left, top):
+        # 执行父类的方法
+        super().__init__()
+        self.screen = screen  # 坦克移动的时候需要显示的屏幕
+        self.direction = 'D'  # 坦克的方向,默认的方向是向下 方向是上下左右
+        self.images = {}  # 坦克的所有方向 key = 方向 value = 图片 (surface)
+        self.images['L'] = pygame.image.load("../images/p1tankL.gif")
+        self.images['R'] = pygame.image.load("../images/p1tankR.gif")
+        self.images['U'] = pygame.image.load("../images/p1tankU.gif")
+        self.images['D'] = pygame.image.load("../images/p1tankD.gif")
+        self.image = self.images[self.direction]  # 坦克的图片 由方向决定
+        self.rect = self.image.get_rect()  # 获取边界
+        self.rect.left = left
+        self.rect.top = top
+        self.live = True  # 决定坦克是否被消灭了
+
+    def display(self):
+        self.image = self.images[self.direction]
+        self.screen.blit(self.image, self.rect)
 
 
 if __name__ == '__main__':
