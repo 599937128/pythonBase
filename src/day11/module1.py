a8a72e671b39a84746d3bd517cfaf256b32daef9
lvshao <599937128@163.com>
2020-10-26 21:35:43 +0800

    坦克位置移动

--- src/day11/module1.py
+++ src/day11/module1.py
@@ -4,40 +4,48 @@ from pygame.locals import *
 
 # 坦克大战的主窗口
 class TankeMain(object):
+    # 屏幕的宽度和高度
+    width = 600
+    height = 500
+
     # 开始游戏的窗口
     def startGame(self):
         # 模块初始化,加载操作系统的资源
         pygame.init()
         # (600,400) 宽高像素 ,第二个参数,窗口的属性 可变 RESIZABLE 0 固定大小, 第三个参数 颜色的位数 32为
-        screen = pygame.display.set_mode((600, 400), 0, 32)
+        screen = pygame.display.set_mode((TankeMain.width, TankeMain.height), 0, 32)
         # 给窗口设置一个标题
         pygame.display.set_caption('坦克大战')
-        my_tank = Tank(screen, 250, 290)
+        my_tank = My_Tank(screen)
         while True:
             # EGB color (0,100,200) 设置屏幕的背景色
             screen.fill((0, 0, 0))
             # 显示左上角的文字
             screen.blit(self.write_text(), (0, 10))
             # 获取事件
-            self.get_event()
+            self.get_event(my_tank)
             my_tank.display()
             # 显示重置
             pygame.display.update()
 
     # 获取所有的事件
-    def get_event(self):
+    def get_event(self, my_tank):
         for event in pygame.event.get():
             if event.type == QUIT:
                 self.stopGame()  # 程序退出
             if event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                    pass
-                if event.key == K_RIGHT:
-                    pass
+                if event.key == K_LEFT or event.key == K_a:
+                    my_tank.direction = 'L'
+                    my_tank.move()
+                if event.key == K_RIGHT or event.key == K_d:
+                    my_tank.direction = 'R'
+                    my_tank.move()
                 if event.key == K_UP:
-                    pass
+                    my_tank.direction = 'U'
+                    my_tank.move()
                 if event.key == K_DOWN:
-                    pass
+                    my_tank.direction = 'D'
+                    my_tank.move()
                 # 敲击esc键
                 if event.key == K_ESCAPE:
                     self.stopGame()
@@ -52,7 +60,7 @@ class TankeMain(object):
 
     # 在屏幕的左上角显示文字内容
     def write_text(self):
-        font = pygame.font.SysFont("simsunexttb", 20)  # 定义一个字体
+        font = pygame.font.SysFont("SimHei", 20)  # 定义一个字体
         text_sf = font.render("敌方坦克数量:10", True, (255, 0, 0))  # 显示文字
         return text_sf
 
@@ -74,6 +82,7 @@ class Tank(BaseItem):
         super().__init__()
         self.screen = screen  # 坦克移动的时候需要显示的屏幕
         self.direction = 'D'  # 坦克的方向,默认的方向是向下 方向是上下左右
+        self.speed = 5  # 坦克移动的速度
         self.images = {}  # 坦克的所有方向 key = 方向 value = 图片 (surface)
         self.images['L'] = pygame.image.load("../images/p1tankL.gif")
         self.images['R'] = pygame.image.load("../images/p1tankR.gif")
@@ -88,6 +97,39 @@ class Tank(BaseItem):
     def display(self):
         self.image = self.images[self.direction]
         self.screen.blit(self.image, self.rect)
+
+    def move(self):
+        pass
+
+    def fire(self):
+        pass
+
+
+class My_Tank(Tank):
+    def __init__(self, screen):
+        super().__init__(screen, 250, 290)  # 创建一个我方坦克
+
+    def move(self):
+        if self.direction == 'L':
+            if self.rect.left > 0:  # 判断是否在屏幕的左边的边界上
+                self.rect.left -= self.speed
+            else:
+                self.rect.left = 0
+        elif self.direction == 'R':
+            if self.rect.right < TankeMain.width:
+                self.rect.right += self.speed
+            else:
+                self.rect.right = TankeMain.width
+        elif self.direction == 'U':
+            if self.rect.top > 0:
+                self.rect.top -= self.speed
+            else:
+                self.rect.top = 0
+        elif self.direction == 'D':
+            if self.rect.top < TankeMain.height:
+                self.rect.top += self.speed
+            else:
+                self.rect.top = TankeMain.height
 
 
 if __name__ == '__main__':
