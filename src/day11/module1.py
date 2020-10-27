365c44b6ba0befd341fee08706811687b6d35f6f
lvshao <599937128@163.com>
2020-10-27 21:57:45 +0800

    我方坦克连续移动和敌方坦克随即移动功能

--- src/day11/module1.py
+++ src/day11/module1.py
@@ -1,12 +1,13 @@
-import pygame, sys
+import pygame, sys, time
+from random import randint
 from pygame.locals import *
 
 
 # 坦克大战的主窗口
 class TankeMain(object):
     # 屏幕的宽度和高度
-    width = 600
-    height = 500
+    width = 800
+    height = 600
 
     # 开始游戏的窗口
     def startGame(self):
@@ -17,6 +18,9 @@ class TankeMain(object):
         # 给窗口设置一个标题
         pygame.display.set_caption('坦克大战')
         my_tank = My_Tank(screen)
+        enemy_list=[]
+        for i in range(1,6):
+            enemy_list.append(Enemy_Tank(screen))
         while True:
             # EGB color (0,100,200) 设置屏幕的背景色
             screen.fill((0, 0, 0))
@@ -24,7 +28,12 @@ class TankeMain(object):
             screen.blit(self.write_text(), (0, 10))
             # 获取事件
             self.get_event(my_tank)
-            my_tank.display()
+            my_tank.display()  # 显示我方坦克
+            my_tank.move()  # 坦克移动
+            for enemy in enemy_list:
+                enemy.display()
+                enemy.randmon_move()
+            time.sleep(0.05)  # 每次休眠0.05秒跳到下一帧
             # 显示重置
             pygame.display.update()
 
@@ -36,19 +45,26 @@ class TankeMain(object):
             if event.type == KEYDOWN:
                 if event.key == K_LEFT or event.key == K_a:
                     my_tank.direction = 'L'
-                    my_tank.move()
+                    my_tank.stop = False
+                    # my_tank.move()
                 if event.key == K_RIGHT or event.key == K_d:
                     my_tank.direction = 'R'
-                    my_tank.move()
+                    my_tank.stop = False
+                    # my_tank.move()
                 if event.key == K_UP:
                     my_tank.direction = 'U'
-                    my_tank.move()
+                    my_tank.stop = False
+                    # my_tank.move()
                 if event.key == K_DOWN:
                     my_tank.direction = 'D'
-                    my_tank.move()
+                    my_tank.stop = False
+                    # my_tank.move()
                 # 敲击esc键
                 if event.key == K_ESCAPE:
                     self.stopGame()
+            if event.type == KEYUP:
+                if event.key == K_LEFT or event.key == K_RIGHT or event.key == K_UP or event.key == K_DOWN:
+                    my_tank.stop = True
 
     # 停止游戏
     def stopGame(self):
@@ -92,6 +108,7 @@ class Tank(BaseItem):
         self.rect = self.image.get_rect()  # 获取边界
         self.rect.left = left
         self.rect.top = top
+        self.rect.bottom = top
         self.live = True  # 决定坦克是否被消灭了
 
     def display(self):
@@ -99,37 +116,79 @@ class Tank(BaseItem):
         self.screen.blit(self.image, self.rect)
 
     def move(self):
-        pass
+        if not self.stop:  # 如果坦克不是停止状态
+            if self.direction == 'L':
+                if self.rect.left > 0:  # 判断是否在屏幕的左边的边界上
+                    self.rect.left -= self.speed
+                else:
+                    self.rect.left = 0
+            elif self.direction == 'R':
+                if self.rect.right < TankeMain.width:
+                    self.rect.right += self.speed
+                else:
+                    self.rect.right = TankeMain.width
+            elif self.direction == 'U':
+                if self.rect.top > 0:
+                    self.rect.top -= self.speed
+                else:
+                    self.rect.top = 0
+            elif self.direction == 'D':
+                if self.rect.bottom < TankeMain.height:
+                    self.rect.bottom += self.speed
+                else:
+                    self.rect.bottom = TankeMain.height
 
     def fire(self):
         pass
 
 
+# 我方坦克
 class My_Tank(Tank):
     def __init__(self, screen):
         super().__init__(screen, 250, 290)  # 创建一个我方坦克
+        self.stop = Tank
 
-    def move(self):
-        if self.direction == 'L':
-            if self.rect.left > 0:  # 判断是否在屏幕的左边的边界上
-                self.rect.left -= self.speed
+
+# 敌方坦克
+class Enemy_Tank(Tank):
+    def __init__(self, screen):
+        super().__init__(screen, randint(1, 5) * 100, 200)
+        self.images = {}  # 坦克的所有方向 key = 方向 value = 图片 (surface)
+        self.images['L'] = pygame.image.load("../images/enemy1L.gif")
+        self.images['R'] = pygame.image.load("../images/enemy1R.gif")
+        self.images['U'] = pygame.image.load("../images/enemy1U.gif")
+        self.images['D'] = pygame.image.load("../images/enemy1D.gif")
+        self.speed = 3
+        self.step = 6 # 坦克按照某个方向移动的步骤
+        self.get_random_direction()
+
+    def get_random_direction(self):
+        r = randint(0, 4)  # 得到一个坦克移动方向和停止方向的随机数
+        if r == 4:
+            self.stop = True
+        elif r == 1:
+            self.direction = 'L'
+            self.stop = False
+        elif r == 2:
+            self.direction = 'R'
+            self.stop = False
+        elif r == 0:
+            self.direction = 'D'
+            self.stop = False
+        elif r == 3:
+            self.direction = 'U'
+            self.stop = False
+    # 地方坦克 按照一个随即的方向 连续移动 6步 然后才能改变方向
+    def randmon_move(self):
+        if self.live:
+            if self.step == 0:
+                self.get_random_direction()
+                self.step = 6
             else:
-                self.rect.left = 0
-        elif self.direction == 'R':
-            if self.rect.right < TankeMain.width:
-                self.rect.right += self.speed
-            else:
-                self.rect.right = TankeMain.width
-        elif self.direction == 'U':
-            if self.rect.top > 0:
-                self.rect.top -= self.speed
-            else:
-                self.rect.top = 0
-        elif self.direction == 'D':
-            if self.rect.top < TankeMain.height:
-                self.rect.top += self.speed
-            else:
-                self.rect.top = TankeMain.height
+                self.move()
+                self.step-=1
+
+
 
 
 if __name__ == '__main__':
